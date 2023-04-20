7a7ee4a11f0f1713924cf4471444ca23d550b61a
Laia Grobe De Luca <laia.gdla@gmail.com>
2023-04-20 20:58:30 +0200

    r

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,3 +4,4 @@ class Review < ApplicationRecord
   validates :content, presence: true
   validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (0..5).to_a }
 end
+
